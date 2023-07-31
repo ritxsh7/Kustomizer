b0ecfbe833e36baf2029f1d603d9a26c60c1e4a5
Ritesh Patil <ritessshhh7@gmail.com>
2023-07-31 19:37:53 +0530

    first commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "http://localhost:8080/api/v1/ritesh",
+  },
+  production: {
+    backendUrl: "https://devswag.onrender.com/api/v1/ritesh",
+  },
+};
+
+export default config;
